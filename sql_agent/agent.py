65c0345c3db5140969a72757fe69249b3ada534b
Maninder Singh <manindersingh221@gmail.com>
2024-02-19 15:02:20 +0530

    added open api version

--- sql_agent/agent.py
+++ sql_agent/agent.py
@@ -41,15 +41,15 @@ def get_agent_llm(agent_llm_name: str):
 
 
 def create_agent(
-    tool_llm_name: str = "gpt-4-1106-preview",
-    agent_llm_name: str = "gpt-4-1106-preview",
+    tool_llm_name: str = "gpt-3.5-turbo",
+    agent_llm_name: str = "gpt-3.5-turbo",
 ):
     """
     Creates a SQL agent using the specified tool and agent LLM names.
 
     Args:
-        tool_llm_name (str, optional): The name of the SQL toolkit LLM. Defaults to "gpt-4-1106-preview".
-        agent_llm_name (str, optional): The name of the agent LLM. Defaults to "gpt-4-1106-preview".
+        tool_llm_name (str, optional): The name of the SQL toolkit LLM. Defaults to "gpt-3.5-turbo".
+        agent_llm_name (str, optional): The name of the agent LLM. Defaults to "gpt-3.5-turbo".
 
     Returns:
         agent: The created SQL agent.
